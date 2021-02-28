8c1a7ffbf2577702207f66556b419e1afcd39fe8
Ivan Kovalev <shoker444@gmail.com>
2021-02-28 15:53:06 +0300

    Add comments

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <netinet/in.h>
 int main (int argc, char *argv[]) {
-        bool is_dnsname = false;
-        bool valid_args = false;
-        struct sockaddr_in sa;
-        struct hostent *he;
+        bool is_dnsname = false; // Boolean for check if dns name promted or ip address
+        bool valid_args = false; // Boolean for check if args are valid
+        struct sockaddr_in sa;   // Stuct which holds ip address. More in netinet/in.h. https://www.gta.ufrj.br/ensino/eel878/sockets/sockaddr_inman.html
+        struct hostent *he;	 // Stuct which holds ip info from gethostbyname. More in netdb.h. https://www.gta.ufrj.br/ensino/eel878/sockets/gethostbynameman.html
         printf("%s\n", "Ping starts");
         if (argc < 2) {
                 printf("%s\n", "You have not specified adress to send ICMP packets");
